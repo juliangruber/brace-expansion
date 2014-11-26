3a2730136ccae206a96ba8fe8d92a6c54d656cdd
isaacs <i@izs.me>
2014-11-26 00:13:17 -0800

    Add bash comparison tests

---
+++ test/bash-comparison.js
@@ -0,0 +1,25 @@
+var test = require('tape');
+var expand = require('..');
+var fs = require('fs');
+var resfile = __dirname + '/bash-results.txt';
+var cases = fs.readFileSync(resfile, 'utf8').split('><><><><');
+
+// throw away the EOF marker
+cases.pop()
+
+test('matches bash expansions', function(t) {
+  cases.forEach(function(testcase) {
+    var set = testcase.split('\n');
+    var pattern = set.shift();
+    var actual = expand(pattern);
+
+    // If it expands to the empty string, then it's actually
+    // just nothing, but Bash is a singly typed language, so
+    // "nothing" is the same as ""
+    if (set.length === 1 && set[0] === '')
+      set = []
+
+    t.same(actual, set, pattern);
+  });
+  t.end();
+})
