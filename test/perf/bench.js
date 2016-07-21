2dd0af277e58cd891d88994ec7db2d80fb247655
Steve Mao <maochenyan@gmail.com>
2016-07-21 21:46:50 +1000

    increase iterations for bench, produce more accurate results

--- test/perf/bench.js
+++ test/perf/bench.js
@@ -4,8 +4,12 @@ var fs = require('fs');
 var resfile = __dirname + '/../cases.txt';
 var cases = fs.readFileSync(resfile, 'utf8').split('\n');
 
-bench('Average', function() {
-  cases.forEach(function(testcase) {
-    expand(testcase);
+suite('bash expansions', function () {
+  set('iterations', 2000);
+
+  bench('Average', function() {
+    cases.forEach(function(testcase) {
+      expand(testcase);
+    });
   });
 });
