ed9572036d57f5a6397996539c5de196d8827a61
Julian Gruber <julian@juliangruber.com>
2022-06-08 18:04:25 +0200

    switch from tape to test module

--- bench/bench.js
+++ bench/bench.js
@@ -1,7 +1,7 @@
 'use strict';
-const expand = require('../../');
+const expand = require('..');
 const fs = require('fs');
-const resfile = __dirname + '/../cases.txt';
+const resfile = __dirname + '/../test/cases.txt';
 const cases = fs.readFileSync(resfile, 'utf8').split('\n');
 
 bench('Average', function() {
