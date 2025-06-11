8229e6f6642729fff02f327bc6c14103113f1c6b
Julian Gruber <julian@juliangruber.com>
2025-06-11 09:03:58 +0200

    fmt

--- test/redos.js
+++ test/redos.js
@@ -3,13 +3,10 @@ import assert from 'assert'
 import expand from '../index.js'
 
 test('redos', function () {
-let str = "{a}" + ",".repeat(100000) + "\u0000";
-    let startTime = performance.now();
-    expand(str)
-    let endTime = performance.now();
-    let timeTaken = endTime - startTime;
-    assert.ok(timeTaken < 1000, `Expected time (${timeTaken}ms) to be less than 1000ms`);
+  const str = '{a}' + ','.repeat(100000) + '\u0000'
+  const startTime = performance.now()
+  expand(str)
+  const endTime = performance.now()
+  const timeTaken = endTime - startTime
+  assert.ok(timeTaken < 1000, `Expected time (${timeTaken}ms) to be less than 1000ms`)
 })
-
-
-
