c3c73c8b088defc70851843be88ccc3af08e7217
mmmsssttt404 <931121963@qq.com>
2025-06-11 15:02:59 +0800

    Fix potential ReDoS Vulnerability or Inefficient Regular Expression (#65)
    * Create redos.js
    
    * Update index.js
    
    * Update test/redos.js
    
    ---------
    
    Co-authored-by: Julian Gruber <julian@juliangruber.com>

---
+++ test/redos.js
@@ -0,0 +1,15 @@
+import test from 'node:test'
+import assert from 'assert'
+import expand from '../index.js'
+
+test('redos', function () {
+let str = "{a}" + ",".repeat(100000) + "\u0000";
+    let startTime = performance.now();
+    expand(str)
+    let endTime = performance.now();
+    let timeTaken = endTime - startTime;
+    assert.ok(timeTaken < 1000, `Expected time (${timeTaken}ms) to be less than 1000ms`);
+})
+
+
+
