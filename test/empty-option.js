a6fec7e7b2c6c8ec0d4123d8d9bb5cc9ee8a5329
isaacs <i@izs.me>
2026-02-03 08:20:37 -0800

    refactor tests

--- test/empty-option.js
+++
@@ -1,9 +0,0 @@
-import test from 'node:test'
-import assert from 'assert'
-import { expand } from '../dist/esm/index.js'
-
-test('empty option', function () {
-  assert.deepStrictEqual(expand('-v{,,,,}'), [
-    '-v', '-v', '-v', '-v', '-v'
-  ])
-})
--- test/order.js
+++
@@ -1,9 +0,0 @@
-import test from 'node:test'
-import assert from 'assert'
-import { expand } from '../dist/esm/index.js'
-
-test('order', function () {
-  assert.deepStrictEqual(expand('a{d,c,b}e'), [
-    'ade', 'ace', 'abe'
-  ])
-})
