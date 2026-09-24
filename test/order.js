af57fa9410cfd76c541bc366282a9b1c1fdc3577
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2026-09-24 23:22:47 +1200

    refactor: apply prettier (#145)
    * chore: setup prettier
    
    * refactor: format with Prettier
    
    * refactor: use es5 rules for trailing commas
    
    * chore: add dependency and scripts
    
    * ci: run prettier
    
    * Update .github/workflows/ci.yml
    
    ---------
    
    Co-authored-by: Julian Gruber <julian@juliangruber.com>

--- test/order.js
+++ test/order.js
@@ -1,10 +1,7 @@
-var test = require('tape');
-var expand = require('..');
+var test = require('tape')
+var expand = require('..')
 
-test('order', function(t) {
-  t.deepEqual(expand('a{d,c,b}e'), [
-    'ade', 'ace', 'abe'
-  ]);
-  t.end();
-});
-
+test('order', function (t) {
+  t.deepEqual(expand('a{d,c,b}e'), ['ade', 'ace', 'abe'])
+  t.end()
+})
--- test/unbound-recursion.js
+++ test/unbound-recursion.js
@@ -1,6 +1,5 @@
-var test = require('tape');
-var expand = require('..');
-
+var test = require('tape')
+var expand = require('..')
 
 // https://github.com/juliangruber/brace-expansion/security/advisories/GHSA-3jxr-9vmj-r5cp
 test('unbound recursion', async t => {
@@ -16,7 +15,7 @@ test('unbound recursion', async t => {
   t.deepEqual(expanded, [str], 'does not expand')
   t.ok(
     timeTaken < 1000,
-    `Expected time (${timeTaken}ms) to be less than 1000ms`,
+    `Expected time (${timeTaken}ms) to be less than 1000ms`
   )
   t.end()
 })
