b8b0deef3abbc686feb63d690429c7433eff5b45
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2026-09-25 21:04:40 +1200

    refactor: apply prettier (#146)
    * chore: setup prettier
    
    * refactor: format with Prettier
    
    * refactor: use es5 rules for trailing commas
    
    * chore: add dependency and scripts
    
    * ci: run prettier
    
    * ci: run on v1 branch

--- test/dollar.js
+++ test/dollar.js
@@ -1,14 +1,17 @@
-var test = require('tape');
-var expand = require('..');
+var test = require('tape')
+var expand = require('..')
 
-test('ignores ${', function(t) {
-  t.deepEqual(expand('${1..3}'), ['${1..3}']);
-  t.deepEqual(expand('${a,b}${c,d}'), ['${a,b}${c,d}']);
+test('ignores ${', function (t) {
+  t.deepEqual(expand('${1..3}'), ['${1..3}'])
+  t.deepEqual(expand('${a,b}${c,d}'), ['${a,b}${c,d}'])
   // `${` suppresses expansion of the rest of the string on the 1.x line,
   // unlike 2.x and 5.x where the tail still expands.
-  t.deepEqual(expand('${a,b}${c,d}{e,f}'), ['${a,b}${c,d}{e,f}']);
-  t.deepEqual(expand('${a,b}${c,d}{1..3}'), ['${a,b}${c,d}{1..3}']);
-  t.deepEqual(expand('{a,b}${c,d}${e,f}'), ['a${c,d}${e,f}','b${c,d}${e,f}']);
-  t.deepEqual(expand('x${a,b}x${c,d}x'), ['x${a,b}x${c,d}x']);
-  t.end();
-});
+  t.deepEqual(expand('${a,b}${c,d}{e,f}'), ['${a,b}${c,d}{e,f}'])
+  t.deepEqual(expand('${a,b}${c,d}{1..3}'), ['${a,b}${c,d}{1..3}'])
+  t.deepEqual(expand('{a,b}${c,d}${e,f}'), [
+    'a${c,d}${e,f}',
+    'b${c,d}${e,f}',
+  ])
+  t.deepEqual(expand('x${a,b}x${c,d}x'), ['x${a,b}x${c,d}x'])
+  t.end()
+})
